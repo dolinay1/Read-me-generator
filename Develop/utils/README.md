e7e2f2ba8b59c7160322f8b1f4ed4e9ab044131a
Julian Dolinay <dolinay1@gmail.com>
2020-04-26 00:41:52 -0400

    readme generator working, needs a few small adjustments

---
+++ Develop/utils/README.md
@@ -0,0 +1,50 @@
+
+
+# Project Title:
+Readme Generator
+
+
+
+## Table of Contents
+
+* [Description](#description)
+
+* [Installation](#installation)
+
+* [Tests](#tests)
+
+* [License](#license)
+
+* [Usage](#usage)
+
+* [Contact](#contact)
+
+
+
+# Description
+It generates readmes
+
+
+# Installation
+npm install
+
+
+# Tests
+npm run tests
+
+
+# License
+MIT
+
+
+# How to contribute
+contact me first
+
+
+# Usage Information
+nothing in particular
+
+
+# Contact
+Questions about the repo or project? Contact at dolinay1 or directly by email at dolinay1@gmail.com.
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,8 +1,44 @@
-function generateMarkdown(data) {
-  return `
-# ${data.title}
 
-`;
-}
+
+// async function init() {
+//   console.log("hi")
+//   try {
+//     const data = await promptUser();
+
+//     const markdown = generateMarkdown(data);
+
+//     await writeFileAsync("Readme.md", markdown);
+
+//     console.log("Successfully wrote Readme");
+//   } catch(err) {
+//     console.log(err);
+//   }
+// }
+
+// init();
+
+// function generateMarkdown(data) {
+//   return `
+
+// # ${data.title}
+
+// # ${data.description}
+
+// # ${data.installation}
+
+// # ${data.tests}
+
+// # ${data.license}
+
+// # ${data.contributing}
+
+// # ${data.usage}
+
+// # ${data.github}
+
+// # ${data.email}
+
+// `;
+// }
 
 module.exports = generateMarkdown;
